422730d31f84fc0ab620a748b62fca96d583f47c
lrosato <rosatoluca2001@gmail.com>
2022-11-03 19:24:15 -0300

    Added all the endpoints for the CRUD of a user.

--- src/route/student.js
+++ src/route/student.js
@@ -30,17 +30,46 @@ router.get("/students/:id", (req, res) => {
         .catch((err) => res.json(err));
 });
 
+// Get student by Email
+router.get("/students/email/:email", (req, res) => {
+    const {email} = req.params;
+    studentSchema
+        .find({email})
+        .then((data) => res.json(data))
+        .catch((err) => res.json(err));
+});
+
+// Get student by phone
+router.get("/students/phone/:phone", (req, res) => {
+    const {phone} = req.params;
+    studentSchema
+        .find({phone})
+        .then((data) => res.json(data))
+        .catch((err) => res.json(err));
+});
+
 // Update student by ID ( PathVariable )
 router.put("/students/:id", (req, res) => {
     const {id} = req.params;
-    const {firstName, lastName, age, email} = req.body;
+    const {firstName, lastName, email, phone, education} = req.body;
     studentSchema
         .updateOne(
             {_id: id},
-            {$set: {firstName, lastName, age, email}}
+            {$set: {firstName, lastName, email, phone, education}}
         )
         .then((data) => res.json(data))
         .catch((err) => res.json(err));
+});
+
+// Update student password by id, email and password
+router.put("/students/password/:id/:email/:password", (req, res) => {
+    const {id, email, password} = req.params;
+    const {newPassword} = req.body;
+    studentSchema
+        .updateOne({_id: id, email , password}, {$set: {password: newPassword}})
+        .then((data) => res.json(data))
+        .catch((err) => res.json(err));
+
 });
 
 // Delete student by ID ( PathVariable )
