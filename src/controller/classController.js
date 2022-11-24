2bef9b674af4482a03449daaad3bf8af40bef598
Facundo Pietra <fpietra@gmail.com>
2022-11-24 13:38:03 -0300

    mas correcciones

--- src/controller/classController.js
+++ src/controller/classController.js
@@ -3,24 +3,29 @@ const classService = require("../service/classService");
 
 const createClass = async function (req, res){
     console.log("Creating class...")
-    const Class = {
-        
-    }
-    const classSchema = classSchema(req.body);
+
+    const newClass = classSchema(req.body);
     try{
-        const createdStudent = await StudentService.createStudent(Student);
-        return res.status(201).json({status: 201, data: createdStudent, message: "Successfully created student"});
+        const createdClass = await classService.createClass(newClass);
+        return res.status(201).json({status: 201, data: createdClass, message: "Successfully created class"});
     } catch(e){
         return res.status(400).json({status: 400, message: e.message});
     }
 
 }
 
-const getAllClasses = (req, res) => {
-    classSchema
-        .find()
-        .then((data) => res.json(data))
-        .catch((err) => res.json(err));
+exports.getAllClasses = async function (req, res){
+    // Check the existence of the query parameters, If doesn't exists assign a default value
+    var page = req.query.page ? req.query.page : 1
+    var limit = req.query.limit ? req.query.limit : 10;
+    try {
+        var Users = await UserService.getUsers({}, page, limit)
+        // Return the Users list with the appropriate HTTP password Code and Message.
+        return res.status(200).json({status: 200, data: Users, message: "Succesfully Users Recieved"});
+    } catch (e) {
+        //Return an Error Response Message with Code and the Error Message.
+        return res.status(400).json({status: 400, message: e.message});
+    }
 }
 
 const getClassById = (req, res) => {
