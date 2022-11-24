d29d9e9ca1b0a212ce0ef217d2c530125b932612
Facundo Pietra <fpietra@gmail.com>
2022-11-24 17:52:13 -0300

    cambios

--- src/route/comment.js
+++ src/route/comment.js
@@ -1,30 +1,20 @@
 const express = require ("express");
 const commentSchema = require("../model/comment");
+const commentController = require("../controller/commentController");
+const { route } = require("./class");
 
 const router = express.Router();
 
 //create comment
-router.post('/comments', (req, res) =>{
-    const commentToSave = commentSchema(req.body);
-    commentToSave.save()
-        .then((data) => res.json(data))
-        .catch((error)=>res.json({ message: error }));
-});
+router.post('/comments', commentController.createComment);
+
+//get one comment by id
+router.get('/comments/:id', commentController.getCommentById);
 
 //get all comments by classId
-router.get('/comments/classId/:classId', (req, res) =>{
-    const {classId} = req.params;
-    commentSchema.find({classId: classId})
-        .then((data) => res.json(data))
-        .catch((error)=>res.json({ message: error }));
-});
+router.get('/comments/:classId', commentController.getCommentsByClass);
 
 //get all comments by classOwnerId
-router.get('/comments/classOwnerId/:classOwnerId', (req, res) => {
-    const { classOwnerId } = req.params;
-    commentSchema.find({classOwnerId: classOwnerId})
-        .then((data) => res.json(data))
-        .catch((error)=>res.json({ message: error }));
-});
+router.get('/comments/classOwnerId/:classOwnerId', commentController.getCommentsByOwner);
 
 module.exports = router;
