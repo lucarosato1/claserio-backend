1f5b807fd8eb29896c0798fde44440854e4c18c4
Facundo Pietra <fpietra@gmail.com>
2022-11-28 16:48:20 -0300

    endpoints para reservas

---
+++ src/controller/reserveController.js
@@ -0,0 +1,45 @@
+const reserveService = require('../service/reserveService');
+
+exports.createReserve = async function (req, res){
+    console.log("Creating reserve...")
+    const reserve = req.body;
+    try{
+        const createdReserve = await reserveService.createReserve(reserve);
+        return res.status(201).json({status: 201, data: createdReserve, message: "Successfully created reserve"});
+    } catch(e){
+        return res.status(400).json({status: 400, message: e.message});
+    }
+}
+
+exports.getReservesByTeacherId = async function (req, res){
+    const {teacherId} = req.params.teacherId;
+    console.log(teacherId);
+    try{
+        const reserves = await reserveService.getReservesByTeacherId(teacherId);
+        return res.status(200).json({status: 200, data: reserves, message: "Successfully reserves retrieved"});
+    } catch(e){
+        return res.status(400).json({status: 400, message: e.message});
+    }    
+}
+
+exports.getReservesByStudentId = async function (req, res){
+    const {studentId} = req.params.studentId;
+    console.log(studentId);
+    try{
+        const reserves = await reserveService.getReservesByStudentId(studentId);
+        return res.status(200).json({status: 200, data: reserves, message: "Successfully reserves retrieved"});
+    } catch(e){
+        return res.status(400).json({status: 400, message: e.message});
+    }    
+}
+
+exports.updateReserve = async function (req, res){
+    const id = req.params.id;
+    const reserve = req.body;
+    try{
+        const updatedReserve = await reserveService.updateReserve(id, reserve);
+        return res.status(200).json({status: 200, data: updatedReserve, message: "Successfully reserve updated"});
+    } catch(e){
+        return res.status(400).json({status: 400, message: e.message});
+    }    
+}
