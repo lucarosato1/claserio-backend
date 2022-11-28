ed9798980ea7804ca66d2c38d1137745ad0b72cc
Facundo Pietra <fpietra@gmail.com>
2022-11-27 23:22:36 -0300

    corrijo route de class

--- src/route/class.js
+++ src/route/class.js
@@ -10,8 +10,10 @@ router.post('/class', classController.createClass);
 //get all classes
 router.get('/class', classController.getAllClasses);
 //get class by id
-router.get('/class/:id', classController.deleteClassById);
+router.get('/class/:id', classController.getClassById);
 //update class by id
 router.put('/class/:id', classController.updateClassById);
+//delete class by id
+router.delete('/class/:id', classController.deleteClassById);
 
 module.exports = router;
