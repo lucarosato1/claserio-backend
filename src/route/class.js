6eca7313ecbd12cb692653dc58f0ee85061c5188
Facundo Pietra <fpietra@gmail.com>
2022-11-27 23:25:34 -0300

    corrijo route de class

--- src/route/class.js
+++ src/route/class.js
@@ -1,17 +1,17 @@
 const express = require ("express");
-const classSchema = require("../model/class");
 const classController = require("../controller/classController");
 
 const router = express.Router();
 
 //create class
 router.post('/class', classController.createClass);
-
 //get all classes
 router.get('/class', classController.getAllClasses);
 //get class by id
-router.get('/class/:id', classController.deleteClassById);
+router.get('/class/:id', classController.getClassById);
 //update class by id
 router.put('/class/:id', classController.updateClassById);
+//delete class by id
+router.delete('/class/:id', classController.deleteClassById);
 
 module.exports = router;
